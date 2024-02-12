3ddd9ded2b65fa84c2315aec860183519a0fd712
Haris Nadeem <haris.nadeem.bsc@gmail.com>
2024-02-12 10:10:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,20 @@ xcode-select -–install;
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ############################################################
+# Set up zsh environment
+############################################################
+
+# 1. Install oh-my-zsh as a zshell plugin manager
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 2.Install custom zsh plugins and themes
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+git clone https://github.com/jimeh/zsh-peco-history.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-peco-history
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+############################################################
 # Next install brew dependencies
 ############################################################
 
@@ -43,6 +57,7 @@ cp -r .config/nvim ~/.config/.;
 ############################################################
 
 ENV NODE_VERSION=16.13.0;
+# ENV NODE_VERSION=18.16.1;
 
 brew uninstall --force node
 brew uninstall --ignore-dependencies node
@@ -61,26 +76,45 @@ nvm alias default v${NODE_VERSION};
 # Set up local python environment
 ############################################################
 
-pyenv install 3.7.13
-pyenv global 3.7.13
+pyenv install 3.9.18
+pyenv global 3.9.18
 
 ############################################################
-# Set up local zsh environment
+# Set up Java Environment
 ############################################################
 
-# 1. Install oh-my-zsh as a zshell plugin manager
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install to manage multiple java versions
+brew install jenv
 
-# 2.Install custom zsh plugins and themes
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-git clone https://github.com/jimeh/zsh-peco-history.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-peco-history
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Install java 11 and link to jenv
+brew install openjdk@11
+sudo ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+jenv add /Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home/
 
-# 3. Backup .zshrc and replace it with new one
+# Install java8 on arm and link to jenv
+brew install --cask homebrew/cask-versions/adoptopenjdk8
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
+
+# Setup jenv plugin compatibility
+jenv enable-plugin export
+jenv enable-plugin maven
+jenv enable-plugin gradle
+
+############################################################
+# Setup scala
+############################################################
+
+curl -s "https://get.sdkman.io" | bash
+sdk install sbt
+sdk install scala 2.12.18
+
+############################################################
+# Set up local custom environment
+############################################################
+
+# 1. Backup .zshrc and replace it with new one
 mv ~/.zshrc ~/.zshrc_backup;
 mv .zshrc ~/.zshrc;
 
-# 4. Copy custom shell functions written
+# 2. Copy custom shell functions written
 cp -r .my-zsh-functions ~/.
